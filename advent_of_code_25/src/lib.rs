651c2a37393a5936c1ae9074e53dc82fcfd3740a
Otto Moen <otto.moen@combine.se>
2025-12-05 22:14:59 +0100

    Add solution for day 4

--- advent_of_code_25/src/lib.rs
+++ advent_of_code_25/src/lib.rs
@@ -1,11 +1,13 @@
 pub mod day1;
 pub mod day2;
 pub mod day3;
+pub mod day4;
 
 type Solver = fn();
 
-pub const SOLVERS: [&[Solver; 2]; 3] = [
+pub const SOLVERS: [&[Solver; 2]; 4] = [
     &[day1::part1, day1::part2],
     &[day2::part1, day2::part2],
     &[day3::part1, day3::part2],
+    &[day4::part1, day4::part2],
 ];
